1e8114ebc76b6a864978c2c32889a07371439e1f
Md. Rakibul Hasan <75192444+rakib181@users.noreply.github.com>
2022-05-07 00:46:58 +0600

    Create Teamplate.java

---
+++ Teamplate.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+public class Template{
+    public static void main(String[] args) {
+        MyScanner sc = new MyScanner(); 
+        out = new PrintWriter(new BufferedOutputStream(System.out)); 
+                //CODE HERE
+              
+        
+        
+        
+
+           out.close();
+            }
+
+
+    //-----------PrintWriter for faster output---------------------------------
+    public static PrintWriter out;
+
+    //-----------MyScanner class for faster input----------
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
+
+}
